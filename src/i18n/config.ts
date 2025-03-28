68ca0e372a0940e94b7fc180971b954ef31f3114
Yousef Elsayed <yousef4511@outlook.com>
2025-03-28 14:28:20 +0200

    feat: set arabic as default locale

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -21,6 +21,7 @@ export const localeToProfile = {
     langTag: "ar-EG", // Extremly important used in localizing dates, numbers and sitemap,  only English alphabet and hyphen allowed
     direction: "rtl", // UI layout direction
     googleFontName: "Cairo", // For OG image generation, font must support 400 and 700 weights, write name as it should goes in a URL, words separated with '+' instead of spaces
+    default: true,
   },
   en: {
     name: "English",
@@ -28,7 +29,6 @@ export const localeToProfile = {
     langTag: "en-US",
     direction: "ltr",
     googleFontName: "IBM+Plex+Mono",
-    default: true,
   },
 } satisfies Record<string, LocaleProfile>;
 
